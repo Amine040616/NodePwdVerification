8c3304a0b1bc8f9ec05376a4602ac27e1262b716
Amine040616 <aminekacem@gmail.com>
2018-09-07 16:50:28 +0000

    email & pwd verification

--- index.js
+++ index.js
@@ -14,17 +14,45 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-/*
-app.post('/', (req, res) => {
-    //console.log(req.body())
-    req.body.timestamp = Math.floor(new Date().getTime()/1000);
-    res.send(req.body)
-})*/
 
-
-   
+// Validation adresse mail :
 
 app.post('/', (req, res) => {
+     
+    var identify = req.body.identifiant;
+    var found = "Votre Identifiant est invalide";
+    var evo = 0;
+    var valid = 0;
+    var reponse = "";
+
+    //var forme = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+
+    for (var i = 0; i < identify.length; i++) {
+        
+        if (identify[i] == "@" && i>1){
+            evo = i;
+            valid = valid + 1;
+            console.log("Charactére est : " + identify[i] + " à la position : " +i);
+        }
+        if (identify[i] == "." && i>evo+3){
+            evo = i;
+            valid = valid + 1;
+            console.log("Charactére est : " + identify[i] + " à la position : " +i);
+        }
+        if (valid == 2 && i>evo+2){
+            console.log("Charactére est : " + identify[i] + " à la position : " +i);
+            found = "Votre Identifiant est valider";
+        }  
+    }
+
+
+
+    // Validation MDP :
+
+
+
+    if (found == "Votre Identifiant est valider"){
+
     var nimrou = ["@","_","-",".",",",";",":"];
     var chaine = req.body.password;
     var power = 0;
@@ -58,73 +86,14 @@ app.post('/', (req, res) => {
             }   
         }
 
+        reponse = found + "  et votre mot de passe du niveau : " + powerMessages[power];
                 
+    }}else{
+        reponse = found;
     }
-    res.send(powerMessages[power]);
-    /*
-
-
-
-    for (var i = 0; i < chaine.length; i++) {
-        var numerik = isNaN(chaine[i]);
-        console.log(numerik);
-
-        if(numerik == false && chaine[i] == chaine[i].toUpperCase()){
-            found = "Mot de passe est normal";
-            console.log(found);
-            break;
-        }
-    }
-
-    for (var i = 0; i < chaine.length; i++) {
-        var numerik = isNaN(chaine[i]);
-        console.log(numerik);
-        
-        if (numerik == false) {
-        console.log(chaine[i]);
-        found = "Mot de passe est faible";
-        console.log(found);
-        break;
-        }
-    }
-
-
-
-
-for (var i = 0; i < chaine.length; i++) {
-         if (nimrou.indexOf(chaine[i]) > -1) {
-            console.log(chaine[i]);
-            found = "Le numero trouver est : " + chaine[i] + " dans la position : " + i;
-            break;
-            //found++;
-         } 
-
-    /*
-    for (var i = 0; i < chaine.length; i++) {
-        var numerik = isNaN(chaine[i]);
-        console.log(numerik);
-        if (numerik == false) {
-           console.log(chaine[i]);
-           var found = "Le numero trouver est : " + chaine[i] + " dans la position : " + i;
-           console.log(found);
-           //found++;
-           break;
-        } 
-        
-   }*/
-   
-    /*
-    var what = nimrou.indexOf(chaine);
-    console.log(what);
-    console.log(chaine);
-    if(what > 0){
-        res.send("sa77itek");
-        console.log(req.body.password);
-    } else {
-        res.send("mouch sa77itek");
-        console.log(req.body.password);
-    }
-    */
+    
+    //res.send(found + "  et votre mot de passe du niveau : " + powerMessages[power]);
+    res.send(reponse);
 })
 
 app.get('/app', (req, res) => res.send('Hello mouch World!'))
